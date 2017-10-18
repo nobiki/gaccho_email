ae9f5bc6ccd22d5e49962d4b45150a429134b290
Naoaki Obiki <obiki.wk@gmail.com>
2017-10-18 15:35:27 +0900

    init.

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup, find_packages
+
+setup(
+        name             = 'gaccho_email',
+        version          = '0.0.1',
+        description      = 'Gaccho Email Plugin',
+        license          = 'MIT',
+        author           = 'nobiki',
+        author_email     = 'test@example.com',
+        url              = 'https://github.com/nobiki/gaccho_email.git',
+        keywords         = 'gaccho email',
+        packages         = find_packages(),
+        install_requires = [],
+        )
+
